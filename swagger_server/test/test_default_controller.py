c65b10cec5eda3f5e7c2a05f6261d4d73be57c30
skoulouzis <S.Koulouzis@uva.nl>
2020-01-08 23:46:48 +0100

    first commit

---
+++ swagger_server/test/test_default_controller.py
@@ -0,0 +1,90 @@
+# coding: utf-8
+
+from __future__ import absolute_import
+
+from flask import json
+
+from swagger_server.models.student import Student  # noqa: E501
+from swagger_server.test import BaseTestCase
+
+
+class TestDefaultController(BaseTestCase):
+    """DefaultController integration test stubs"""
+
+    def test_add_student(self):
+        """Test case for add_student
+        Add a new student
+        """
+        body = Student()
+        body.first_name = 'Bender'
+        body.last_name = 'Rodriguez'
+        body.grades = {'math': 8, 'history': 9}
+        response = self.client.open(
+            '/service-api/student',
+            method='POST',
+            data=json.dumps(body),
+            content_type='application/json')
+        self.assert200(response,
+                       'Response body is : ' + response.data.decode('utf-8'))
+        self.assertTrue(response.is_json)
+        self.assertIsInstance(response.json, int)
+
+    def test_delete_student(self):
+        """Test case for delete_student
+        """
+        body = Student()
+        body.first_name = 'Bender'
+        body.last_name = 'Rodriguez'
+        body.grades = {'math': 8, 'history': 9}
+        response = self.client.open(
+            '/service-api/student',
+            method='POST',
+            data=json.dumps(body),
+            content_type='application/json')
+        student_id = (response.json)
+        # response = self.client.open(
+        #     '/service-api/student/{student_id}'.format(student_id=student_id),
+        #     method='DELETE')
+        # self.assert200(response,
+        #                'Response body is : ' + response.data.decode('utf-8'))
+        # 
+        # response = self.client.open(
+        #     '/service-api/student/{student_id}'.format(student_id=-1),
+        #     method='DELETE')
+        # self.assert404(response,
+        #                'Response body is : ' + response.data.decode('utf-8'))
+
+
+    def test_get_student_by_id(self):
+        """Test case for get_student_by_id
+
+        Find student by ID
+        """
+        body = Student()
+        body.first_name = 'Bender'
+        body.last_name = 'Rodriguez'
+        body.grades = {'math': 8, 'history': 9}
+        response = self.client.open(
+            '/service-api/student',
+            method='POST',
+            data=json.dumps(body),
+            content_type='application/json')
+        student_id = (response.json)
+
+        query_string = [('math', 9)]
+        response = self.client.open(
+            '/service-api/student/{student_id}'.format(student_id=789),
+            method='GET',
+            query_string=query_string)
+        self.assert200(response,
+                       'Response body is : ' + response.data.decode('utf-8'))
+        self.assertTrue(response.is_json)
+        self.assertIsInstance(response.json, dict)
+
+
+
+
+if __name__ == '__main__':
+    import unittest
+
+    unittest.main()
